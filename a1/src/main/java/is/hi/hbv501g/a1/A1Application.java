5062ab52550df13af0621ac3d9d12a539ca33a0a
Logi <logi.arg@gmail.com>
2024-09-08 21:25:28 +0000

    Initial commit

---
+++ a1/src/main/java/is/hi/hbv501g/a1/A1Application.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.a1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A1Application.class, args);
+	}
+
+}
---
+++ a1/src/test/java/is/hi/hbv501g/a1/A1ApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.a1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
